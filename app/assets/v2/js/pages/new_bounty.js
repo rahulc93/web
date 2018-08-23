09c4b586a6a4601e00e19f0ee01499daf35fe0e2
Owocki <ksowocki@gmail.com>
2018-08-23 15:10:50 -0600

    not enough token warning

--- app/assets/v2/js/pages/new_bounty.js
+++ app/assets/v2/js/pages/new_bounty.js
@@ -292,6 +292,9 @@ $(document).ready(function() {
       var isETH = tokenAddress == '0x0000000000000000000000000000000000000000';
       var token_contract = web3.eth.contract(token_abi).at(tokenAddress);
       var account = web3.eth.coinbase;
+      if(!isETH){
+        check_balance_and_alert_user_if_not_enough(tokenAddress, amount);
+      }
 
       amount = amount * decimalDivisor;
       // Create the bounty object.
@@ -421,3 +424,23 @@ $(document).ready(function() {
     }
   });
 });
+
+var check_balance_and_alert_user_if_not_enough = function(tokenAddress, amount){
+    var token_contract = web3.eth.contract(token_abi).at(tokenAddress);
+    var from = web3.eth.coinbase;
+    var token_details = tokenAddressToDetails(tokenAddress);
+    var token_decimals = token_details['decimals'];
+    var token_name = token_details['name'];
+    token_contract.balanceOf.call(from, function(error, result) {
+      if(error) return;
+      var balance = result.toNumber() / Math.pow(10, 18);
+      var balance_rounded = Math.round(balance * 10 ) / 10;
+      if(parseFloat(amount) > balance){
+        var msg = gettext("You do not have enough tokens to fund this bounty. You have ") + balance_rounded + " " + token_name + " " + gettext(" but you need ") + amount + " " + token_name;
+        _alert(msg, 'warning');
+      }
+    });
+
+
+}
+
